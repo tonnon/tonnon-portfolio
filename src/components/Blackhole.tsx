72e057bafcbd827a4c017dd8a6859a75ce6af915
tonnon <lucastonnon@gmail.com>
2025-03-26 06:34:51 -0300

    blackhole

--- src/components/Blackhole.tsx
+++ src/components/Blackhole.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Video() {
+  const [videoLoaded, setVideoLoaded] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setVideoLoaded(true), 100); // Pequeno delay para suavizar o efeito
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <div className="fixed top-[-385px] left-0 w-full h-screen flex justify-center items-center z-[-1] overflow-hidden">
       <video 
         autoPlay
         muted
         loop
-        className="rotate-180 w-full h-full object-cover opacity-80"
+        preload="auto"
+        className={`rotate-180 w-full h-full object-cover opacity-0 transition-opacity duration-1000 ${videoLoaded ? 'opacity-80' : ''}`}
       >
         <source 
           src="/blackhole.mp4" 
